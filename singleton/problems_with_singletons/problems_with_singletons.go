d6d219b77e713d81708233e85db2f0769e4cb028
gianvittorio <gianvittorio.castellano@recargapay.com>
2021-12-04 18:52:08 -0300

    done w singleton

---
+++ singleton/problems_with_singletons/problems_with_singletons.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+)
+
+type Database interface {
+	GetPopulation(name string) int
+}
+
+type singletonDatabase struct {
+	capitals map[string]int
+}
+
+func (db *singletonDatabase) GetPopulation(name string) int {
+	return db.capitals[name]
+}
+
+// sync.Once init() -- thread safety
+// lazyness
+
+var once sync.Once
+//var instance Database
+var instance *singletonDatabase
+
+func readData(path string) (map[string]int, error) {
+	ex, err := os.Executable()
+	if err != nil {
+	  panic(err)
+	}
+	exPath := filepath.Dir(ex)
+  
+	file, err := os.Open(exPath + path)
+	if err != nil {
+	  return nil, err
+	}
+	defer file.Close()
+  
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+  
+	result := map[string]int{}
+	
+	for scanner.Scan() {
+	  k := scanner.Text()
+	  scanner.Scan()
+	  v, _ := strconv.Atoi(scanner.Text())
+	  result[k] = v
+	}
+  
+	return result, nil
+  }
+
+func GetSingletonDabatase() *singletonDatabase {
+	once.Do(func() {
+		caps, e := readData("capitals.txt")
+		db := singletonDatabase{caps}
+
+		if e == nil {
+			db.capitals = caps
+		}
+
+		instance = &db
+	})
+
+	return instance
+}
+
+func GetTotalPopulation(cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += GetSingletonDabatase().GetPopulation(city)
+	}
+	return result
+}
+
+func main() {
+	cities := []string{"Seoul", "Mexico City"}
+	tp := GetTotalPopulation(cities)
+
+	ok := tp == (17500000 + 17400000)
+	fmt.Println(ok)
+}
---
+++ singleton/singleton_and_dependency_inversion/singleton_and_dependency_inversion.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+)
+
+type Database interface {
+	GetPopulation(name string) int
+}
+
+type singletonDatabase struct {
+	capitals map[string]int
+}
+
+func (db *singletonDatabase) GetPopulation(name string) int {
+	return db.capitals[name]
+}
+
+// sync.Once init() -- thread safety
+// lazyness
+
+var once sync.Once
+//var instance Database
+var instance *singletonDatabase
+
+func readData(path string) (map[string]int, error) {
+	ex, err := os.Executable()
+	if err != nil {
+	  panic(err)
+	}
+	exPath := filepath.Dir(ex)
+  
+	file, err := os.Open(exPath + path)
+	if err != nil {
+	  return nil, err
+	}
+	defer file.Close()
+  
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+  
+	result := map[string]int{}
+	
+	for scanner.Scan() {
+	  k := scanner.Text()
+	  scanner.Scan()
+	  v, _ := strconv.Atoi(scanner.Text())
+	  result[k] = v
+	}
+  
+	return result, nil
+  }
+
+func GetSingletonDabatase() *singletonDatabase {
+	once.Do(func() {
+		caps, e := readData("capitals.txt")
+		db := singletonDatabase{caps}
+
+		if e == nil {
+			db.capitals = caps
+		}
+
+		instance = &db
+	})
+
+	return instance
+}
+
+func GetTotalPopulation(cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += GetSingletonDabatase().GetPopulation(city)
+	}
+	return result
+}
+
+func GetTotalPopulationEx(db Database, cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += db.GetPopulation(city)
+	}
+	return result
+}
+
+type DummyDatabase struct {
+	dummyData map[string]int
+}
+
+func (d *DummyDatabase) GetPopulation(name string) int {
+	if len(d.dummyData) == 0 {
+		d.dummyData = map[string]int {
+			"alpha" : 1,
+			"beta" : 2,
+			"gamma" : 3,
+		}
+	}
+
+	return d.dummyData[name]
+}
+
+func main() {
+	cities := []string{"alpha", "gamma"}
+	tp := GetTotalPopulationEx(&DummyDatabase{}, cities)
+
+	fmt.Println(tp == 4)
+}
